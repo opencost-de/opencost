e4fa828a4f93a5177adcf01894562dca03452db2
cbroschinski <broschinski@uni-bielefeld.de>
2026-09-23 10:28:25 +0200

    cleanup + linting

--- doc/json/tools/convert_to_json_schema.py
+++ doc/json/tools/convert_to_json_schema.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+"""
+Basic converter to transform the openCost XSD schema into a JSON schema.
+
+This is no general JSON to XSD converter and only implements a specific 
+subset of XSD element combinations used in openCost. It also relies on all
+types definitions being stored as top-level elements in the XSD.
+"""
+
 import argparse
 import json
 import sys
@@ -9,11 +17,13 @@ import xml.etree.ElementTree as ET
 from collections import OrderedDict
 
 class UnsupportedXSDStructure(Exception):
-    
-    def __init__(self, type_element, parent, children_allowed):
-        elem_name = type_element.attrib["name"]
-        msg = "Type '{}': Only the following children of {} are currently supported: {}"
-        msg = msg.format(elem_name, parent, ", ".join(children_allowed))
+
+    def __init__(self, type_element, children_allowed):
+        elem_name = type_element.attrib.get("name")
+        elem_name = "(" + elem_name + ")" if elem_name is not None else ""
+        elem_tag = type_element.tag
+        msg = "Element '{}' {}: Only the following children of this element are currently supported: {}"
+        msg = msg.format(elem_tag, elem_name, ", ".join(children_allowed))
         super().__init__(msg)
 
 HELP_MSGS = {
@@ -39,10 +49,12 @@ json_schema = OrderedDict({
 # 2) Convert all SimpleTypes to JSON Schema Structures
 # 3) Start constructing the JSON Schema recursively from the TLE
 # 4) When encountering a type definition, convert the corresponding XSD type to JSON. Apply recursion if encountering more types during the process.
-# 5) Store completely converted JSON structures in a lookup table. 
+# 5) Store completely converted complex types in a lookup table.
 #
-# Additional insights:
-# In contrast to XML child elements, JSON objects properties are unordered. This means we can treat <xs:all> and <xs:sequence> as identical.
+# Additional remarks:
+# - sequence/choice structures in the openCost XSD are usually applied to allow free element ordering.
+# - In contrast to XML child elements, JSON objects properties are unordered. This means we can usually treat <xs:all> and <xs:sequence> as identical.
+# - Converting these directly may lead to very complicated allOf/OneOf JSON constructs. If possible, these should be simplified after conversion.
 
 # XSD types converted to JSON structures
 
@@ -75,7 +87,7 @@ def convert_element(elem, tle=False):
         else:
             ret = type_structure
     return ret
-    
+
 def convert_sequence_indicator(seq_elem):
     ret = OrderedDict({
         "type": "object",
@@ -90,12 +102,12 @@ def convert_sequence_indicator(seq_elem):
         required_list = []
         for element in elements:
             name = element.attrib["name"]
-            minOccurs = element.attrib.get("minOccurs", "1")
-            maxOccurs = element.attrib.get("maxOccurs", "1")
-            maxOccursNumber = 1
-            if maxOccurs.isdecimal():
-                maxOccursNumber = int(maxOccurs)
-            if maxOccurs == "unbounded" or maxOccursNumber > 1:
+            min_occurs = element.attrib.get("minOccurs", "1")
+            max_occurs = element.attrib.get("maxOccurs", "1")
+            max_occurs_number = 1
+            if max_occurs.isdecimal():
+                max_occurs_number = int(max_occurs)
+            if max_occurs == "unbounded" or max_occurs_number > 1:
                 ret["properties"][name] = OrderedDict({
                     "type": "array",
                     "minItems": 1,
@@ -103,7 +115,7 @@ def convert_sequence_indicator(seq_elem):
                 })
             else:
                 ret["properties"][name] = convert_element(element)
-            if minOccurs == "0" or sequence_skippable:
+            if min_occurs == "0" or sequence_skippable:
                 pass
             else:
                 required_list.append(OrderedDict({"required": [name]}))
@@ -133,11 +145,11 @@ def convert_sequence_indicator(seq_elem):
                 else:
                     ret["allOf"] = [choice_structure["allOf"]]
             elif "anyOf" in choice_structure:
-                anyOf = OrderedDict({"anyOf": choice_structure["anyOf"]})
+                any_of = OrderedDict({"anyOf": choice_structure["anyOf"]})
                 if "allOf" in ret:
-                    ret["allOf"] += anyOf
+                    ret["allOf"] += any_of
                 else:
-                    ret["allOf"] = [anyOf]
+                    ret["allOf"] = [any_of]
     return ret
 
 def convert_all_indicator(all_elem):
@@ -151,9 +163,9 @@ def convert_all_indicator(all_elem):
         required_list = []
         for element in elements:
             name = element.attrib["name"]
-            minOccurs = element.attrib.get("minOccurs", "1")
+            min_occurs = element.attrib.get("minOccurs", "1")
             ret["properties"][name] = convert_element(element)
-            if minOccurs == "0":
+            if min_occurs == "0":
                 pass
             else:
                 required_list.append(OrderedDict({"required": [name]}))
@@ -167,15 +179,15 @@ def convert_choice_indicator(choice_elem):
         "additionalProperties": False,
         "properties": OrderedDict(),
     })
-    maxOccurs = choice_elem.attrib.get("maxOccurs", "1")
-    maxOccursNumber = 1
-    if maxOccurs.isdecimal():
-        maxOccursNumber = int(maxOccurs)
+    max_occurs = choice_elem.attrib.get("maxOccurs", "1")
+    max_occurs_number = 1
+    if max_occurs.isdecimal():
+        max_occurs_number = int(max_occurs)
     elements = choice_elem.findall("xs:element", namespaces)
     sequences = choice_elem.findall("xs:sequence", namespaces)
     if len(elements) > 0:
         ret["oneOf"] = []
-        if maxOccurs == "unbounded" or maxOccursNumber > 1:
+        if max_occurs == "unbounded" or max_occurs_number > 1:
             for element in elements:
                 name = element.attrib["name"]
                 ret["properties"][name] = OrderedDict({
@@ -190,19 +202,21 @@ def convert_choice_indicator(choice_elem):
                 ret["properties"][name] = convert_element(element)
                 ret["oneOf"].append(OrderedDict({"required": [name]}))
     elif len(sequences) > 0:
-        allOfs = []
+        all_ofs = []
         for sequence in sequences:
             seq_structure = convert_sequence_indicator(sequence)
             for name, elem_structure in seq_structure["properties"].items():
                 ret["properties"][name] = elem_structure
             if "allOf" in seq_structure:
-                allOfs.append(seq_structure["allOf"])
-        if len(allOfs) > 0:
-            # If all allOfs dicts are identical, we can merge them into a single structure
-            if allOfs.count(allOfs[0]) == len(allOfs):
-                ret["allOf"] = allOfs[0]
+                all_ofs.append(seq_structure["allOf"])
+        if len(all_ofs) > 0:
+            # If all all_ofs dicts are identical, we can merge them into a single structure
+            if all_ofs.count(all_ofs[0]) == len(all_ofs):
+                ret["allOf"] = all_ofs[0]
             else:
-                ret["anyOf"] = [elem[0] for elem in allOfs]
+                ret["anyOf"] = [elem[0] for elem in all_ofs]
+    else:
+        raise UnsupportedXSDStructure(choice_elem, ["xs:element", "xs:sequence"])
     return ret
 
 def convert_complex_type(complex_element):
@@ -215,13 +229,14 @@ def convert_complex_type(complex_element):
         return convert_all_indicator(all_elem)
     elif seq_elem is not None:
         return convert_sequence_indicator(seq_elem)
-    return {}
+    else:
+        raise UnsupportedXSDStructure(complex_element, ["xs:choice", "xs:all", "xs:sequence"])
 
 def convert_simple_type(simple_element):
     ret = OrderedDict()
     res_elem = simple_element.find("xs:restriction", namespaces)
     if res_elem is None:
-        raise UnsupportedXSDStructure(simple_element, "xs:simpleType", ["xs:restriction"])
+        raise UnsupportedXSDStructure(simple_element, ["xs:restriction"])
     type_restriction = RESTRICTION_BASE_TYPES.get(res_elem.attrib.get("base", ""), None)
     if type_restriction is not None:
         ret["type"] = type_restriction
@@ -240,39 +255,42 @@ def convert_simple_type(simple_element):
         value = minlength.attrib["value"]
         ret["minLength"] = int(value)
     else:
-        raise UnsupportedXSDStructure(simple_element, "xs:restriction", ["xs:enumeration", "xs:pattern", "xs:minLength"])
+        raise UnsupportedXSDStructure(res_elem, ["xs:enumeration", "xs:pattern", "xs:minLength"])
     return ret
 
-parser = argparse.ArgumentParser()
-parser.add_argument("xsd_files", nargs="+", help=HELP_MSGS["xsd_files"])
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("xsd_files", nargs="+", help=HELP_MSGS["xsd_files"])
+    args = parser.parse_args()
 
-top_level_element = None
+    top_level_element = None
 
-for xsd_file in args.xsd_files:
-    tree = ET.parse(xsd_file)
-    root = tree.getroot()
-    complex_type_elems = tree.findall("./xs:complexType", namespaces)
-    for comp_type_elem in complex_type_elems:
-        type_name = comp_type_elem.attrib["name"]
-        XSD_COMPLEX_TYPES[type_name] = comp_type_elem
-    simple_type_elems = tree.findall("./xs:simpleType", namespaces)
-    for simp_type_elem in simple_type_elems:
-        type_name = simp_type_elem.attrib["name"]
-        XSD_SIMPLE_TYPES[type_name] = simp_type_elem
-    tle = tree.find("./xs:element", namespaces)
-    if tle is not None:
-        if top_level_element is None:
-            top_level_element = tle
-        else:
-            print("ERROR: More than one top-level xs:element found!")
-            sys.exit()
+    for xsd_file in args.xsd_files:
+        tree = ET.parse(xsd_file)
+        complex_type_elems = tree.findall("./xs:complexType", namespaces)
+        for comp_type_elem in complex_type_elems:
+            type_name = comp_type_elem.attrib["name"]
+            XSD_COMPLEX_TYPES[type_name] = comp_type_elem
+        simple_type_elems = tree.findall("./xs:simpleType", namespaces)
+        for simp_type_elem in simple_type_elems:
+            type_name = simp_type_elem.attrib["name"]
+            XSD_SIMPLE_TYPES[type_name] = simp_type_elem
+        tle = tree.find("./xs:element", namespaces)
+        if tle is not None:
+            if top_level_element is None:
+                top_level_element = tle
+            else:
+                print("ERROR: More than one top-level xs:element found!")
+                sys.exit()
 
-for type_name, simp_type_elem in XSD_SIMPLE_TYPES.items():
-    CONVERTED_TYPES[type_name] = convert_simple_type(simp_type_elem)
+    for type_name, simp_type_elem in XSD_SIMPLE_TYPES.items():
+        CONVERTED_TYPES[type_name] = convert_simple_type(simp_type_elem)
 
-json_schema["properties"] = convert_element(top_level_element, tle=True)
-json_schema["required"] = [top_level_element.attrib["name"]]
+    json_schema["properties"] = convert_element(top_level_element, tle=True)
+    json_schema["required"] = [top_level_element.attrib["name"]]
 
-with open("json_schema_out.json", "w") as out:
+    with open("json_schema_out.json", "w", encoding="utf-8") as out:
         out.write(json.dumps(json_schema, indent=2))
+
+if __name__ == '__main__':
+    main()
